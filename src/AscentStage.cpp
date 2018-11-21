5b136ffeb0a1055f8b9016de6744d95d6d568263
Jonas <jonas.colve@outlook.de>
2018-11-21 15:57:36 +0100

    Added Stagging for AscentStage

--- src/AscentStage.cpp
+++ src/AscentStage.cpp
@@ -3,7 +3,11 @@
 AscentStage::AscentStage(const std::string& name, std::shared_ptr<MissionInfo> info) : MissionStage(name, info) {}
 
 MissionStageStatus AscentStage::update() {
-	if (activate())
+	if (activate()) {
+		m_info->vessel->control().set_throttle(1);
+		m_info->vessel->auto_pilot().engage();
+	}
+	if (m_info->vessel->resources_in_decouple_stage(m_info->vessel->control().current_stage()).amount("LiquidFuel") < 0.1)
 		m_info->vessel->control().activate_next_stage();
-	return MissionStageStatus::Completed;
+	return MissionStageStatus::InProgress;
 }
